fccb22025f2e40bde842510c01a2ba5a382c68f6
Katherine Aguilera <katherinam35@gmail.com>
2019-04-23 16:48:43 -0500

    require() and module.exports

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-/**
- * *************
- * Comience aqui
- * *************
- */
+// (rquiere) : ruta del modulo que quiera importar
+const math = require ("./math");
+// no esta el directorio directamente pero de igual manera lo busca por defecto como index.js
+const greet = require("./greetings");
+const hello = require("./greetings/hello");
+const numbers = require("./number");
+
+
+//  var math . add function de math.js
+console.log(math.add (4, 5));
+console.log(math.divide (4, 5));
+console.log(math.substract (4, 5));
+console.log(math.multiply (4, 5));
+
+// greet var.greet function de greetings/index.js
+console.log (greet.greet("Kathy"));
+console.log (hello.sayHello("Winny"));
+
+console.log (numbers.primo(5));
+
+
---
+++ number.js
@@ -0,0 +1,18 @@
+module.exports = {
+  primo: numero => {
+      console.log(`Has pasado el numero: ${numero}`);
+      console.log(`Inicio bucle desde 2 hasta ${(numero - 1)}`);
+
+      for (var i = 2; i < numero; i++) {
+          console.log(`Modificador entre ${numero} y ${i} = ${(numero % i)}`);
+          if (numero % i === 0) {
+            console.log(`${i} es un multiplo de ${numero}`);
+            console.log(`${numero} no es un numero primo porque ${i} es un multiplo`);
+            return false;
+          }
+      }
+      if (numero === 1) console.log("Me has pasado el numero 1, recuerda que NO es un numero primo");
+      else console.log("Como el numero ingresado no tuvo mas múltiplos entonces determinamos que SI es un numero primo.");
+      console.log("-------------------------------------");
+  }
+}
