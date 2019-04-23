0a9d9858b4683c6f05327dc3b242ce9e13e8e1c9
Katherine Aguilera <katherinam35@gmail.com>
2019-04-23 17:15:48 -0500

    modules externos

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+//llamando modulo de node_modules
+const cowsay = require("cowsay")
+
+// funcion del modulo / y parametros
+console.log(cowsay.say({
+    text: "Hello Kathy",
+    e: "o o",
+    T: "U"
+  })
+);
+
