fdff2a0835caea846a93119a5f22db05571c93de
Katherine Aguilera <katherinam35@gmail.com>
2019-04-23 21:35:40 -0500

    Async

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const add = require("./src/addition");
+const sim = require("./src/intensive");
+
+add.syncSum(1,2);
+add.asyncSum(4,2);
+
+sim.simulateAsync();
+// sim.simulateSync();
