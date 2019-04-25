bb5310e8156bb828df2f1a009de2612fa5de9128
Katherine Aguilera <katherinam35@gmail.com>
2019-04-25 11:50:27 -0500

    async await

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+// call.greet("Kate Griz")
+async function callwithPromise(){
+  const fullName = await call.withPromise("Kate", "Griz")
+  console.log(fullName);
+}
+callwithPromise();
