8d6bb2294dbe513c3ccbc398fb654620e78ddbbb
Katherine Aguilera <katherinam35@gmail.com>
2019-04-25 13:34:46 -0500

    child_process

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require("child_process");
+// Mostar lista de carpeta y archivos
+function execComand(command){
+  cp.exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.log(`Error: ${err}`); return;
+      }
+      if (stdout) console.log(`Standard out: \n${stdout}`);
+      if (stderr) console.log(`Standard out: \n${stdout}`);
+    })
+}
+
+// execComand("ls");
+// funcion en script de multiplicar
+execComand("node ./src/script --base=6");
