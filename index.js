8d57c567e8ce537516b45c33d00d6d4e681f4572
Katherine Aguilera <katherinam35@gmail.com>
2019-04-25 16:34:18 -0500

    Events

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const greet = require("./src/greet")
+
+ greet.emit("clap") //Hacer llamado al evento sin argumento
+ greet.emit("greet", "CC") //Hacer llamado al evento, con un argumento
+
+ greet.emit("call", "Carlos Castañeda" ,name=> { //Llamado a evento con función Callback
+     console.log(`Hi! ${name}`)
+ })
