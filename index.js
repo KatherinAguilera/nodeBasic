d7ad8fd0867a0b4ad3fa007069f63334a72c4f4d
Katherine Aguilera <katherinam35@gmail.com>
2019-04-25 17:04:34 -0500

    server static

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require('http');
+const fs = require('fs');
+
+// servidor metodo de node request, response
+const server = http.createServer((req, res) => {
+  // lectura de archivo
+	fs.readFile("./resources/my_page.html", (err, data) => {
+    if(err) {console.error(err); return; }
+		res.end(data);
+	});
+});
+console.log("Iniciando servidor");
+server.listen(3000);
