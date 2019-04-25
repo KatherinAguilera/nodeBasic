4faff67f5f9272a151712415f72ce28ec7496ef5
Katherine Aguilera <katherinam35@gmail.com>
2019-04-24 20:42:11 -0500

    Callback

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require('./src/call');
+
+// console.log(call.sync("Katherine Aguilera"));
+
+// call.withCallback("Jimmy Hanse", call.sync);
+
+call.withPromise("Ken Steek")
+  .then(name=>{ console.log(name) });
+
+
+// call.withPromise( 'Mario', call.sync, call.withCallback(call.bienvenidoCallback) ).then( stat => {stat;} );>
