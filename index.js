96c02985b6869dae8f201685d2befd1252f97bef
Katherine Aguilera <katherinam35@gmail.com>
2019-04-25 17:31:38 -0500

    Server with express

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require(`express`);
+const app = express();
+
+/* GET http://localhost:3000 */
+app.get(`/`, (req, res) => {
+  res.send(`Estoy en la ruta HOME`);
+});
+
+/* GET http://localhost:3000/app */
+app.get("/app", function(req, res, next) {
+  console.log(req.query.filtro);
+  if (req.query.filtro) {
+    next();
+    return;
+  }
+  res.send(`Estoy en la ruta APP`);
+});
+
+/* GET http://localhost:3000/app?filtro=JUAN */
+app.get("/app", function(req, res) {
+  let filtro = req.query.filtro;
+  res.send("Get filtro: " + filtro);
+});
+
+/* GET http://localhost:3000/app/1 */
+app.get("/app/:id", function(req, res, next) {
+  let id = req.params.id;
+  res.send("Get id: " + id);
+});
+
+app.get(`*`, (req, res) => {
+  res.send(`No sé dónde estoy.`);
+});
+
+app.listen(3000, () => {
+  console.log(`Express está en escucha en el puerto 3000.`);
+});
